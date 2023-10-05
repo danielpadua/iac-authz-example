a7f53c75f022dd4613a59c272454d0cda336579e
danielpadua <daniel.padua@outlook.com>
2023-10-05 08:19:21 -0300

    Provide unique name for aws s3 bucket

--- iac/main.tf
+++ iac/main.tf
@@ -1,5 +1,8 @@
 resource "aws_s3_bucket" "this" {
-  bucket = "my-tf-test-bucket"
+  bucket = "my-tf-test-bucket-${random_id.this.hex}"
 
   tags = var.tags
+}
+resource "random_id" "this" {
+  byte_length = 8
 }
