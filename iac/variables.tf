1c2fb15500d870fd6ce857423bba69ea4c8a5606
danielpadua <daniel.padua@outlook.com>
2023-10-05 08:10:22 -0300

    Assign provider region to secret variable

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,3 +1,7 @@
+variable "aws_region" {
+  description = "(Required) AWS region in which the resources should be created"
+  type        = string
+}
 variable "tags" {
   description = "(Optional) Default tags"
   type        = map(string)
