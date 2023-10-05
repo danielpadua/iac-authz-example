d824ff7eaa5ad3573ff2a185fe1f8aeb2373ae25
danielpadua <daniel.padua@outlook.com>
2023-10-05 12:36:00 -0300

    Debug versioning and acl tfplan

--- iac/main.tf
+++ iac/main.tf
@@ -4,6 +4,18 @@ resource "aws_s3_bucket" "this" {
   tags = var.tags
 }
 
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
+}
+
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
