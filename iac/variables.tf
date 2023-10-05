726ab7886c1b73a46d82dfbef36c414839e3d0bb
danielpadua <daniel.padua@outlook.com>
2023-10-05 08:15:22 -0300

    Change aws region to default provider variable

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,7 +1,3 @@
-variable "aws_region" {
-  description = "(Required) AWS region in which the resources should be created"
-  type        = string
-}
 variable "tags" {
   description = "(Optional) Default tags"
   type        = map(string)
